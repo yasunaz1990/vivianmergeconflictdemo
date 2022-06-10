5dc35d0b7eace505797ef76828f9aeb0615c7e63
Nijat Muhtar <nijatmn@gmail.com>
2022-06-10 19:33:03 -0400

    initial project

---
+++ src/RegressionTest.java
@@ -0,0 +1,6 @@
+public class RegressionTest {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/SmokeTest.java
@@ -0,0 +1,6 @@
+public class SmokeTest {
+
+    public static void main(String[] args) {
+
+    }
+}
