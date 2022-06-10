46077bb9245a8c8475dd3edc5c3186cc0ee5fc77
Nijat Muhtar <nijatmn@gmail.com>
2022-06-10 19:43:16 -0400

    implmented required smoke test case

--- src/SmokeTest.java
+++ src/SmokeTest.java
@@ -1,6 +1,8 @@
 public class SmokeTest {
 
     public static void main(String[] args) {
-
+        System.out.println("hi");
+        System.out.println("This is a code by Nijat");
+        System.out.println("creating a test case");
     }
 }
