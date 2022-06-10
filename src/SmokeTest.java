d30c8468af216311404f81f5a4f2fe0b62b65da7
Nijat Muhtar <nijatmn@gmail.com>
2022-06-10 19:53:30 -0400

    satisfited with the changes

--- src/SmokeTest.java
+++ src/SmokeTest.java
@@ -2,8 +2,8 @@ public class SmokeTest {
 
     public static void main(String[] args) {
         System.out.println("hi");
-        System.out.println("This is a code by Nijat");
-        System.out.println("code typed by vivian");
+        System.out.println("This is a code by nijat");
+        System.out.println("code typed by VIVIAN");
         System.out.println("creating a test case");
     }
 }
