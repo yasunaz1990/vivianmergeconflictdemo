5a2dae7f943ebcf618895a8d9a78609d18b968a9
Vivian Tejada <VivianTejada12@gmail.com>
2022-06-10 19:40:27 -0400

    implemented smoke test case

--- src/SmokeTest.java
+++ src/SmokeTest.java
@@ -1,6 +1,8 @@
 public class SmokeTest {
 
     public static void main(String[] args) {
-
+        System.out.println("hi");
+        System.out.println("code typed by vivian");
+        System.out.println("creating a test case");
     }
 }
