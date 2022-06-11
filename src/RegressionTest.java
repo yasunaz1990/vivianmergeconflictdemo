98af109665b1e809e7738bf8d5ca052650010d04
Vivian Tejada <VivianTejada12@gmail.com>
2022-06-10 20:06:41 -0400

    Snapshot

--- src/RegressionTest.java
+++ src/RegressionTest.java
@@ -1,6 +1,8 @@
 public class RegressionTest {
 
     public static void main(String[] args) {
-
+        System.out.println("hello");
+        System.out.println("im at the beach");
+        System.out.println("yay");
     }
 }
