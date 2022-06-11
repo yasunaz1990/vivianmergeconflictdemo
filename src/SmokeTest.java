6e603c9b0769c67ef108dcb98e5e60ebaa80ee16
Nijat Muhtar <nijatmn@gmail.com>
2022-06-10 20:04:35 -0400

    latest change

--- src/SmokeTest.java
+++ src/SmokeTest.java
@@ -2,8 +2,8 @@ public class SmokeTest {
 
     public static void main(String[] args) {
         System.out.println("hi");
-        System.out.println("This is a code by nijat");
-        System.out.println("code typed by VIVIAN");
+        System.out.println("This is a code by NIJAT MUHTAR");
+        System.out.println("code typed by VIVIAN student!");
         System.out.println("creating a test case");
     }
 }
